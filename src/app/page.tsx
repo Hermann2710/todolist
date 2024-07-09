f5e542115501fceeaccdcde37203bf8aeccb233f
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2024-07-09 02:23:16 +0100

    Avant la stylisation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,57 @@
+"use client"
+import { useState } from "react";
+
+interface Tache{
+  id: number;
+  text: string;
+}
+
 const Home = () => {
+  const [task, setTask] = useState<string>("");
+  const [tasks, setTasks] = useState<Tache[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTask(e.target.value);
+  };
+
+  const addTask = () => {
+    if (task.trim() === "") {
+      return;
+    }
+    setTasks(prev => {
+      return [...prev, { id: prev.length + 1, text: task }];
+    });
+    setTask("");
+  };
+
+  const deleteTask = (id: number) => {
+    setTasks(prev => prev.filter(t => t.id!== id));
+  }
+
   return (
     <div>
-      Modification
+      <h1>A todo-list</h1>
+      <div>
+        <label htmlFor="task">Add task</label>
+        <input
+          type="text"
+          id="task"
+          placeholder="Add task"
+          value={task}
+          onChange={handleChange}
+        />
+        {task && <button onClick={addTask}>Add</button>}
+        <h3>Tasks List</h3>
+        <ul>
+          {tasks.map(item =>  (
+            <li key={item.id}>{item.text}
+            <button onClick={() => deleteTask(item.id)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
